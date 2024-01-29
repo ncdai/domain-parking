e764ac375e02482eca7f8ec1b015a9047e97cbf3
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-30 01:10:45 +0700

    chore(ecosystem.config.js): rename app name from 'sale-domain' to 'sell-domains' for better clarity and consistency feat(nginx.conf): update server_name to '_' to allow the server to respond to any hostname feat(package.json): add 'mini-svg-data-uri' as a dependency to enable the usage of SVG data URIs style(static/tailwind.css): update body background color and text color for better readability and aesthetics style(static/tailwind.css): add utility classes for pointer-events-none, fixed, absolute, relative, inset-0, left-0, top-0, z-10, flex, h-full, and w-full style(static/tailwind.css): add utility classes for select-none, items-center, and justify-center style(static/tailwind.css): update background colors and gradients for better visual appeal style(static/tailwind.css): add utility classes for text-2xl and text-transparent style(static/tailwind.css): add utility classes for background grid patterns and mask images style(static/tailwind.css): add responsive utility classes for text sizes style(styles/tailwind.css): add base styles for body background color and text color
    chore(tailwind.config.js): add custom colors and utility classes for background images
    chore(tailwind.config.js): add custom font family for display text
    chore(tailwind.config.js): optimize SVG images to use tiny data URIs
    chore(tailwind.config.js): add plugin to generate utility classes for background images
    chore(tailwind.config.js): remove unused code and optimize imports
    
    refactor(domain.ejs): remove unused HTML template file
    
    refactor(home.ejs): update HTML lang attribute to English
    refactor(home.ejs): update page title and meta description to use dynamic domain value
    refactor(home.ejs): remove unused meta tags
    refactor(home.ejs): update container class to fixed position with background and text styling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'sale-domain',
+      name: 'sell-domains',
       script: 'yarn start',
       env: {
         NODE_ENV: 'development'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,55 @@
+const plugin = require('tailwindcss/plugin')
+
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
+const svgToTinyDataUri = require('mini-svg-data-uri');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.{ejs, js, html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#EAF3FF',
+          100: '#CED8E4',
+          200: '#B2BECD',
+          300: '#94A3B7',
+          400: '#7789A2',
+          500: '#5D7088',
+          600: '#48576B',
+          700: '#323E4D', // primary
+          800: '#1D2531',
+          900: '#040D17'
+        }
+      },
+    },
+    fontFamily: {
+      'display': ['Oswald'],
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToTinyDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" },
+      );
+    }),
+  ],
 }
-
