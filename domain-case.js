6ec48c8bca07686bb442cdda2075f491303668b2
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-15 00:29:35 +0700

    refactor(domain-case.js): remove unused domain entries and add new domain entries for better organization and clarity

--- domain-case.js
+++ domain-case.js
@@ -1,9 +1,7 @@
 module.exports = {
   'maytinh.net': 'MayTinh.net',
   'loc.vn': 'Loc.vn',
-  'maytinhviet.com': 'MayTinhViet.com',
   'kysu.vn': 'KySu.vn',
-  'topweb.vn': 'TopWeb.vn',
   'metabox.vn': 'MetaBox.vn',
   't-d.vn': 'T-D.vn',
   'cdx.vn': 'CDX.vn',
@@ -15,7 +13,8 @@ module.exports = {
   'cotienxanh.com': 'CoTienXanh.com',
   'dai0.com': 'dai0.com',
   'cd8.net': 'cd8.net',
-  'id.id.vn': 'id.id.vn',
   'e.id.vn': 'e.id.vn',
-  'e.io.vn': 'e.io.vn'
+  'e.io.vn': 'e.io.vn',
+  'uah.me': 'UAH.me',
+  'chanhdai.io.vn': 'ChanhDai.io.vn',
 }
