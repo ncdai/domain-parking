961081436f4d8f8a0716780b6ac19cd52d04f20e
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-12-12 23:46:25 +0700

    fix(domain-case.js): remove 'hcmcmetro.net' entry and update 'cdx.vn' value for consistency

--- domain-case.js
+++ domain-case.js
@@ -1,6 +1,5 @@
 module.exports = {
-  'hcmcmetro.net': 'HCMC Metro',
   'gameonline.vn': 'GameOnline.vn',
   'gameonline.com.vn': 'GameOnline.com.vn',
-  'cdx.vn': 'CDX: Chuyển Đổi Xanh'
+  'cdx.vn': 'Chuyển Đổi Xanh'
 }
