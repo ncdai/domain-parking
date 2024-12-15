e84ac98d264e6af939eaca86210550898a765c14
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-12-15 21:38:49 +0700

    feat: add domain-specific favicons for enhanced branding
    Introduce domain-icon.js to map domains to their favicons. Update index.js
    to include faviconURL in the response. Add favicon images to static/favicons
    directory. Modify home.ejs to render the favicon link if available. This
    enhancement improves user experience by displaying domain-specific icons.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { getOGImageURL } = require('./og')
 
 const DOMAIN_LIST = require('./domain-list')
 const DOMAIN_CASE = require('./domain-case')
+const DOMAIN_ICON = require('./domain-icon')
 
 const packageJSON = require('./package.json')
 
@@ -32,11 +33,13 @@ const checkDomain = async (req, res, next) => {
 app.get('/', checkDomain, (req, res) => {
   const host = req.hostname
   const domain = DOMAIN_CASE[host] || host
+  const faviconURL = DOMAIN_ICON[host] || 'favicons/default.png'
   const canonicalURL = req.protocol + '://' + req.get('host') + req.originalUrl
   const ogImageURL = getOGImageURL(domain)
 
   res.render('home', {
     domain,
+    faviconURL,
     canonicalURL,
     ogImageURL,
     version: packageJSON.version
