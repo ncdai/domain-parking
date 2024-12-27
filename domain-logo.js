5b8880d7efc3e67bcb8787bb84a0362e22074a80
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-12-27 18:32:18 +0700

    feat(domain-logo.js): add logo for cdx.vn to support new domain branding
    feat(static/logo): add cdx.vn.png to provide visual representation for cdx.vn
    
    chore(static/logo/original): add planet-earth.png for future use or reference

--- domain-logo.js
+++ domain-logo.js
@@ -14,6 +14,7 @@ module.exports = {
 
   'senda.vn': 'logo/senda.vn.png',
   'send.vn': 'logo/send.vn.png',
+  'cdx.vn': 'logo/cdx.vn.png',
 
   'gameonline.vn': 'logo/gameonline.vn.png',
   'gameonline.com.vn': 'logo/gameonline.vn.png',
